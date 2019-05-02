2934a03f1eff7a82ab0448e89b2bce1ea97cafab
amnayak <amukt8@gmail.com>
2019-05-02 15:56:15 -0400

    update test script

--- server/sendData.py
+++ server/sendData.py
@@ -7,13 +7,13 @@ server = (os.getenv('SERVER', 'http://localhost:3001'))
 # use to send fake data
 
 randData = []
-for i in range(144):
-    for j in range(144):
+for i in range(288):
+    for j in range(288):
         randData.append(random.randint(-1, 1)) #push values from [-1, 1]
 
 randPos = {
-    'x' : random.randint(0, 144),
-    'y' : random.randint(0, 144)
+    'x' : random.randint(0, 288),
+    'y' : random.randint(0, 288)
 }
 
 print(randPos)
